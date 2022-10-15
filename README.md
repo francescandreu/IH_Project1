e8ade0a7a2edcb5f3beb0d49d81f92c607230d40
Francesc Andreu <francesc.andreumacias@gmail.com>
2022-10-15 18:28:18 +0200

    Most of data cleaning done

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # IH_Project1
+
+# Title
+
+## Header 1
+
+### Header 2
+
+```python
+print("Hello Wordl")
+```
+
+**bold letters**
+
+*italic letters*
+
+Regular text
+
+dfnwnejfnewrk
---
+++ src/main.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+
+def main():
+    attacks = pd.read_csv("../data/attacks.csv", encoding='unicode_escape')
+    print("Hello world!")
+
+if __name__ == "__main__":
+    main()
